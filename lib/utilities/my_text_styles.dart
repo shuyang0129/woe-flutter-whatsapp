98374ea74e8720154a9af0a0dee0bb9af187703b
shuyang <shuyang0129@gmail.com>
2023-03-27 15:51:19 +0800

    feat: manage custom text styles

---
+++ lib/utilities/my_text_styles.dart
@@ -0,0 +1,326 @@
+import 'package:flutter/material.dart';
+
+class MyTextStyles {
+  // ↓ ↓ ↓ ↓ ↓ ↓ Display Large ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle displayLargeHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 41,
+    height: 48 / 41,
+  );
+
+  static const TextStyle displayLargeRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 41,
+    height: 48 / 41,
+  );
+
+  static const TextStyle displayLargeLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 41,
+    height: 48 / 41,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Display Small ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle displaySmallHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 36,
+    height: 40 / 36,
+  );
+
+  static const TextStyle displaySmallRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 36,
+    height: 40 / 36,
+  );
+
+  static const TextStyle displaySmallLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 36,
+    height: 40 / 36,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Heading 1 ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle f1Heavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+    height: 36 / 32,
+  );
+
+  static const TextStyle f1Regular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 32,
+    height: 36 / 32,
+  );
+
+  static const TextStyle f1Light = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 32,
+    height: 36 / 32,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Heading 2 ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle f2Heavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 29,
+    height: 32 / 29,
+  );
+
+  static const TextStyle f2Regular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 29,
+    height: 32 / 29,
+  );
+
+  static const TextStyle f2Light = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 29,
+    height: 32 / 29,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Heading 3 ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle f3Heavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 26,
+    height: 32 / 26,
+  );
+
+  static const TextStyle f3Regular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 26,
+    height: 32 / 26,
+  );
+
+  static const TextStyle f3Light = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 26,
+    height: 32 / 26,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Heading 4 ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle f4Heavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 23,
+    height: 24 / 23,
+  );
+
+  static const TextStyle f4Regular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 23,
+    height: 24 / 23,
+  );
+
+  static const TextStyle f4Light = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 23,
+    height: 24 / 23,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Heading 5 ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle f5Heavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 24 / 20,
+  );
+
+  static const TextStyle f5Regular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+    height: 24 / 20,
+  );
+
+  static const TextStyle f5Light = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 20,
+    height: 24 / 20,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Heading 6 ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle f6Heavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 20 / 18,
+  );
+
+  static const TextStyle f6Regular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+    height: 20 / 18,
+  );
+
+  static const TextStyle f6Light = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 18,
+    height: 20 / 18,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Paragraph Large ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle paragraphLargeHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+
+  static const TextStyle paragraphLargeRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+
+  static const TextStyle paragraphLargeLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Paragraph Base ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle paragraphBaseHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle paragraphBaseRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle paragraphBaseLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Paragraph Small ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle paragraphSmallHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    height: 20 / 13,
+  );
+
+  static const TextStyle paragraphSmallRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+    height: 20 / 13,
+  );
+
+  static const TextStyle paragraphSmallLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 13,
+    height: 20 / 13,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Paragraph XSmall ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle paragraphXSmallHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 11,
+    height: 16 / 11,
+  );
+
+  static const TextStyle paragraphXSmallRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 11,
+    height: 16 / 11,
+  );
+
+  static const TextStyle paragraphXSmallLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 11,
+    height: 16 / 11,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Paragraph Tiny ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle paragraphTinyHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 9,
+    height: 12 / 9,
+  );
+
+  static const TextStyle paragraphTinyRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 9,
+    height: 12 / 9,
+  );
+
+  static const TextStyle paragraphTinyLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 9,
+    height: 12 / 9,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Small Caps ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle smallCapsHeavy = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 20 / 14,
+    letterSpacing: 14 * 0.05,
+  );
+
+  static const TextStyle smallCapsRegular = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 20 / 14,
+    letterSpacing: 14 * 0.05,
+  );
+
+  static const TextStyle smallCapsLight = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+    height: 20 / 14,
+    letterSpacing: 14 * 0.06,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Button ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle actionButtonLarge = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 28 / 18,
+  );
+
+  static const TextStyle actionButtonMedium = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle actionButtonSmall = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 20 / 14,
+  );
+
+  // ↓ ↓ ↓ ↓ ↓ ↓ Link ↓ ↓ ↓ ↓ ↓ ↓
+
+  static const TextStyle actionLinkLarge = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+
+  static const TextStyle actionLinkMedium = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle actionLinkSmall = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 20 / 14,
+  );
+}
